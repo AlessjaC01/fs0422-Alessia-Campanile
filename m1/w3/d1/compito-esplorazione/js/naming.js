fd86b79c1e47f9ab793f4fd714484fb9684dd4e2
AlessjaC01 <alessjacampanile56@gmail.com>
2022-07-05 11:04:11 +0200

    esplorazione JS

---
+++ m1/w3/d1/compito-esplorazione/js/naming.js
@@ -0,0 +1,27 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var personaggioPreferito = 'undici'
+console.log()
+
+var div = document.getElementsByTagName('div')
+console.log(div)
+div[0].style.backgroundColor = 'blue';
+div[1].style.backgroundColor = 'red';
+div[2].style.backgroundColor = 'purple';
+div[3].style.backgroundColor = 'orange';
+div[4].style.backgroundColor = 'green';
+div[5].style.backgroundColor = 'yellow';
+console.log()
+var personaggioPreferito = document.getElementById('personaggio-preferito')
+console.log()
+
+function saluta(){
+    alert('FriendsDontLie');
+    bloccoRosso.style.backgroundColor = 'blue'
+}
+
+
+//var 12 = 'numero';
