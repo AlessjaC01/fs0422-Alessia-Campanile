f8a6e29d364b4c7f21c69256ccac306ca5a14b7e
AlessjaC01 <alessjacampanile56@gmail.com>
2022-07-08 16:12:22 +0200

    esercizio

---
+++ m1/w3/d4/compito-javascript/js/script.js
@@ -0,0 +1,26 @@
+let fraseOlive = "Non mi piaccioni le olive"
+console.log(fraseOlive)
+let fraseMaiuscola = fraseOlive.toUpperCase()
+console.log(fraseMaiuscola)
+let fraseMinuscola = fraseOlive.toLowerCase()
+console.log(fraseMinuscola)
+splittata = fraseOlive.split(" ")
+console.log(splittata)
+console.log(splittata[3].concat(splittata[4]))
+let personeStranger = ["steve", "max", "mike"]
+console.log(personeStranger[2])
+let personaAggiunta = "undici"
+personeStranger.push(personaAggiunta)
+console.log(personeStranger.length)
+let numeri = []
+numeri.push(Math.floor(Math.random() * 7))
+console.log(numeri)
+let ArrayIoso = [3, 5, 21, 33]
+ArrayIoso.push(44)
+console.log(ArrayIoso)
+ArrayIoso.pop()
+console.log(ArrayIoso)
+ArrayIoso.shift()
+console.log(ArrayIoso)
+ArrayIoso.unshift(3)
+console.log(ArrayIoso)
