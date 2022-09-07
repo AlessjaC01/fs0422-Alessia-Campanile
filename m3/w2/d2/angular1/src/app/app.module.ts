dae1fd810c44be9858a1f8658c80728bbc9d3bab
AlessjaC01 <alessjacampanile56@gmail.com>
2022-09-07 21:23:32 +0200

    modifica compito

--- m3/w2/d2/angular1/src/app/app.module.ts
+++ m3/w2/d2/angular1/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './main/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PostAttiviComponent } from './pages/post-attivi/post-attivi.component';
+import { PostInattiviComponent } from './pages/post-inattivi/post-inattivi.component';
+import { ButtonComponent } from './main/button/button.component';
+import { HeaderComponent } from './main/header/header.component';
+import { FormsComponent } from './main/forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PostAttiviComponent,
+    PostInattiviComponent,
+    ButtonComponent,
+    HeaderComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
